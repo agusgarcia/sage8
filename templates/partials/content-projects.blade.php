01ee6b12cb12ab7b1dc45e186c9c1f4094d81cf6
Agustina <geminis_agus@hotmail.com>
2017-02-13 23:48:24 +0100

    update project page

--- templates/partials/content-projects.blade.php
+++ templates/partials/content-projects.blade.php
@@ -4,8 +4,22 @@
 @if(have_rows('projects'))
     @while(have_rows('projects'))
         @php(@the_row())
-        @php(the_sub_field('project_name'))
-        @php(the_sub_field('project_link'))
-        @php(the_sub_field('project_description'))
+        <div class="project_main">
+            <p>@php(the_sub_field('project_name'))</p>
+            @php(the_sub_field('project_link'))
+        </div>
+        <div class="project_information">
+            <div class="project_information-clipped abs-center">
+                <div>
+                    @php(the_sub_field('project_description'))</div>
+            </div>
+            <svg height="0" width="0">
+                <defs>
+                    <clipPath id="svgPath">
+                        <polygon fill="#EFA400" points="0,109.3 851.5,0 873.2,176.7 632,202.7 886,276.2 472,450.2 "/>
+                    </clipPath>
+                </defs>
+            </svg>
+        </div>
     @endwhile
 @endif
