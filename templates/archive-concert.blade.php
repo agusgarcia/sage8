7b4cebf17bf247ccd5399e5db6fba8800ab1e1e2
Augustina Garcia <agarcia@megalo.fr>
2017-02-16 15:47:03 +0100

    sort concerts by year

--- templates/archive-concert.blade.php
+++ templates/archive-concert.blade.php
@@ -10,20 +10,32 @@
         {!! get_search_form(false) !!}
     @endif
 
-    @php($the_query = new WP_Query(array(
-    'post_type' => 'concert',
-    'annee' => date('Y'),
-    'orderby'   => 'annee',
-	'order' => 'ASC')))
 
-    @if($the_query->have_posts())
-        @while( $the_query->have_posts())
-            @php($the_query->the_post())
-            @include ('partials.content-concert')
-        @endwhile
-    @else
-        <p>Pas de concerts</p>
+    @php($tax = 'taxonomy_annee')
+    @php($terms = get_terms('annee'))
+    @if ($terms != null )
+        <ul>
+            @foreach( $terms as $term )
+                @php($term_name = $term->name)
+                <li> {{ $term_name }} :
+                @php($the_query = new WP_Query(array(
+   'post_type' => 'concert',
+   'annee' => $term_name,
+   'orderby'   => 'date',
+   'order' => 'ASC')))
+
+                @if($the_query->have_posts())
+                    @while( $the_query->have_posts())
+                        @php($the_query->the_post())
+                        @include ('partials.content-concert')
+                    @endwhile
+                @endif
+                </li>
+            @endforeach
+        </ul>
     @endif
+
+
 
     @php($tax = 'taxonomy_annee')
     @php($terms = get_terms('annee'))
