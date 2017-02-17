a51c48e07705712d4373f6c33e7bf8ddff390935
Augustina Garcia <agarcia@megalo.fr>
2017-02-17 17:04:01 +0100

    create concerts.js

--- templates/archive-concert.blade.php
+++ templates/archive-concert.blade.php
@@ -1,8 +1,6 @@
 @extends('layouts.base')
 @section('content')
 
-    @include('partials.page-header')
-
     @if (!have_posts())
         <div class="alert alert-warning">
             {{ __('Sorry, no results were found.', 'sage') }}
@@ -17,19 +15,19 @@
         <ul>
             @foreach( $terms as $term )
                 @php($term_name = $term->name)
-                <li> {{ $term_name }} :
-                @php($the_query = new WP_Query(array(
-   'post_type' => 'concert',
-   'annee' => $term_name,
-   'orderby'   => 'date',
-   'order' => 'ASC')))
+                <li class="concert_year concert_year-{{ $term_name }}"> {{ $term_name }} :
+                    @php($the_query = new WP_Query(array(
+       'post_type' => 'concert',
+       'annee' => $term_name,
+       'orderby'   => 'date',
+       'order' => 'ASC')))
 
-                @if($the_query->have_posts())
-                    @while( $the_query->have_posts())
-                        @php($the_query->the_post())
-                        @include ('partials.content-concert')
-                    @endwhile
-                @endif
+                    @if($the_query->have_posts())
+                        @while( $the_query->have_posts())
+                            @php($the_query->the_post())
+                            @include ('partials.content-concert')
+                        @endwhile
+                    @endif
                 </li>
             @endforeach
         </ul>
@@ -40,10 +38,11 @@
     @php($tax = 'taxonomy_annee')
     @php($terms = get_terms('annee'))
     @if ($terms != null )
+        <h1>Archive ?</h1>
         <ul>
             @foreach( $terms as $term )
                 @php($term_link = get_term_link($term))
-                <li><a href='{{esc_url($term_link)}}'> {{ $term->name }}</a></li>
+                <li data-year="{{$term->name}}"> {{ $term->name }}</li>
             @endforeach
         </ul>
     @endif
